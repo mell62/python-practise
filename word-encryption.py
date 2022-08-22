ef98ca7cb2b8257c00303392060863e3a59ecd82
Mridhul <mridhul67@gmail.com>
2022-08-23 02:16:52 +0530

    Add comment about program

--- word-encryption.py
+++ word-encryption.py
@@ -1,3 +1,4 @@
+#This program uses Caesar cipher
 word=input("Enter a single lowercase word:")
 distance=int(input("Enter the distance value:"))
 code=""
@@ -5,6 +6,6 @@ for letter in word:
     ascii=ord(letter)
     cipher=ascii+distance
     if (cipher>ord("z")):
-        cipher= ord("a") + distance - (ord("z")-ascii+1)
+        cipher= ord("a") + (cipher - ord("z")) - 1
     code+=chr(cipher)
 print(code)
